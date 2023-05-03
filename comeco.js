047bc760721303092b4e3351a09e5d8774562195
MichaelFelipeCabrera <michaelfelipecabrera@gmail.com>
2023-05-02 21:00:16 -0400

    Ifone 12 Vertical

--- comeco.js
+++ comeco.js
@@ -1,11 +1,11 @@
 
 var g = 4;
 window.onload = function () {
-	var u = setInterval(mudarblur, 100);
+	var u = setInterval(mudarblur, 200);
 }
 
 function mudarblur() {
-	g = g - 0.7;
+	g = g - 0.8;
 	document.getElementById("logo_comeco").style.filter = 'blur(' + g + 'px)';
 
 	if (g < -10) {
